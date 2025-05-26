a3e654f42d8601d6406c5428c0d978aac58c24b8
Matteo Parrucci <m.parrucci@unibo.it>
2025-05-26 12:01:38 +0200

    trying to monkeypatch HAS_AT

---
+++ contentimport/__init__.py
@@ -0,0 +1,7 @@
+from collective.exportimport.serializer import HAS_AT as Serializer_HAS_AT
+from collective.exportimport.export_content import HAS_AT as ExportContent_HAS_AT
+from collective.exportimport.export_other import HAS_AT as ExportOther_HAS_AT
+
+Serializer_HAS_AT = False
+ExportContent_HAS_AT = False
+ExportOther_HAS_AT = False
