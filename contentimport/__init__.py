1726eded95921fd2aa1a73222dfe95adc7060252
Matteo Parrucci <m.parrucci@unibo.it>
2025-05-26 12:05:31 +0200

    improt monkeypatch

--- contentimport/__init__.py
+++ contentimport/__init__.py
@@ -1,7 +1,7 @@
-from collective.exportimport.serializer import HAS_AT as Serializer_HAS_AT
-from collective.exportimport.export_content import HAS_AT as ExportContent_HAS_AT
-from collective.exportimport.export_other import HAS_AT as ExportOther_HAS_AT
+from collective.exportimport import serializer
+from collective.exportimport import export_content
+from collective.exportimport import export_other
 
-Serializer_HAS_AT = False
-ExportContent_HAS_AT = False
-ExportOther_HAS_AT = False
+serializer.HAS_AT = False
+export_content.HAS_AT = False
+export_other.HAS_AT = False
